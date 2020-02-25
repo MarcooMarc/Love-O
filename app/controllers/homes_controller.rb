0afcefbb153a6748ccecf24691d355c695476a9e
marc zarb <marc.zarb91@gmail.com>
2020-02-25 20:10:52 +0100

    fix form

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,12 @@
 class HomesController < ApplicationController
-  def index
+  def index1
     @home = Home.all
     @bento = Bento.all.where('id < 4')
   end
 
+  def index2
+  	@parcour = Parcour.all
+  end
 
 end
 
